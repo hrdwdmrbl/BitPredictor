fd5f6148d4f9c0943e90b61900a401ff276f2237
Kevin <kevin@Kevins-MacBook-Air.local>
2013-03-23 10:55:52 -0400

    Commit Android App -- initial template.

---
+++ android/PredictionMarket/src/com/bitcoin/predictionmarket/adapter/ContractListAdapter.java
@@ -0,0 +1,156 @@
+/*******************************************************************************
+ * Copyright (C) 2012 Digipom Inc. All Rights Reserved. Unauthorized distribution is strictly prohibited.
+ ******************************************************************************/
+package com.bitcoin.predictionmarket.adapter;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.bitcoin.predictionmarket.R;
+import com.bitcoin.predictionmarket.adapter.ContractListAdapter.SyncWithServer.SettableList;
+
+public class ContractListAdapter extends BaseAdapter {
+	private final DecimalFormat doubleFormatter = new DecimalFormat("#.##");	
+	private final LayoutInflater layoutInflater;
+	private final ListView listView;		
+
+	private List<ListItem> listData = new ArrayList<ListItem>();
+	
+	private final SettableList settableList = new SettableList() {
+		@Override
+		public void setList(List<ListItem> newListData) {
+			ContractListAdapter.this.setList(newListData);
+		}
+	};
+	
+	static class ContractViewHolder {
+		View contractItemLayout;		
+		TextView contractName;
+		TextView contractPrice;		
+	}
+
+	public abstract static class ListItem {
+
+	}
+
+	public static class ContractListItem extends ListItem {
+		public String contractName;
+		public double contractPrice;
+	}
+	
+	public ContractListAdapter(Context context, ListView listView) {		
+		this.layoutInflater = LayoutInflater.from(context);
+		this.listView = listView;		
+	}		
+
+	@Override
+	public int getCount() {
+		return listData.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return listData.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}	
+
+	public List<ListItem> getList() {
+		return listData;
+	}	
+
+	void setList(List<ListItem> newListData) {				
+		listData = newListData;		
+		notifyDataSetChanged();
+	}	
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {		
+		if (convertView == null) {
+			convertView = layoutInflater.inflate(R.layout.contract_item, null);
+			
+			final ContractViewHolder contractViewHolder = new ContractViewHolder();
+			contractViewHolder.contractItemLayout = convertView.findViewById(R.id.contractItemLayout);
+			contractViewHolder.contractName = (TextView) convertView.findViewById(R.id.contractName);
+			contractViewHolder.contractPrice = (TextView) convertView.findViewById(R.id.contractPrice);
+			
+			convertView.setTag(contractViewHolder);					
+		}		
+		
+		final ContractListItem listItem = (ContractListItem) listData.get(position);
+		final ContractViewHolder viewHolder = (ContractViewHolder) convertView.getTag();
+		
+		viewHolder.contractName.setText(listItem.contractName);
+		viewHolder.contractPrice.setText(doubleFormatter.format(listItem.contractPrice));
+
+		return convertView;
+	}
+
+	public void syncWithServer(OnSyncCompleteListener listener) {
+		new SyncWithServer(listView, listener, settableList).execute();
+	}	
+
+	public interface OnSyncCompleteListener {
+		void onSyncComplete();
+	}
+
+	static class SyncWithServer extends AsyncTask<Void, Void, List<ListItem>> {
+		interface SettableList {
+			void setList(List<ListItem> newListData);
+		}
+		
+		private final ListView listView;
+		private final OnSyncCompleteListener onSyncCompleteListener;
+		private final SettableList settableList;			
+
+		public SyncWithServer(ListView listView,
+				OnSyncCompleteListener onSyncCompleteListener, SettableList settableList) {
+			this.listView = listView;
+			this.onSyncCompleteListener = onSyncCompleteListener;
+			this.settableList = settableList;			
+		}
+
+		@Override
+		protected void onPreExecute() {
+			((TextView) listView.getEmptyView()).setText(R.string.loading);
+		}
+
+		@Override
+		protected List<ListItem> doInBackground(Void... params) {						
+			final List<ListItem> result = new ArrayList<ListItem>();	
+			
+			ContractListItem a = new ContractListItem();
+			a.contractName = "Dow Jones > 25000 by January 2014";
+			a.contractPrice = 4.50;
+			result.add(a);
+			
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(List<ListItem> result) {
+			if (result.isEmpty()) {
+				((TextView) listView.getEmptyView()).setText(R.string.noContractsFound);				
+			}
+
+			settableList.setList(result);
+
+			if (onSyncCompleteListener != null) {
+				onSyncCompleteListener.onSyncComplete();
+			}
+		}
+	}	
+}
---
+++ android/PredictionMarket/src/com/bitcoin/predictionmarket/fragment/ContractListFragment.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (C) 2012 Digipom Inc. All Rights Reserved. Unauthorized distribution is strictly prohibited.
+ ******************************************************************************/
+package com.bitcoin.predictionmarket.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.actionbarsherlock.app.SherlockListFragment;
+import com.bitcoin.predictionmarket.R;
+import com.bitcoin.predictionmarket.adapter.ContractListAdapter;
+import com.bitcoin.predictionmarket.adapter.ContractListAdapter.ListItem;
+import com.bitcoin.predictionmarket.adapter.ContractListAdapter.OnSyncCompleteListener;
+
+public class ContractListFragment extends SherlockListFragment implements OnSyncCompleteListener {
+	public static final String TAG = "ContractListFragment";
+	
+	private ContractListAdapter contractListAdapter;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		final View layout = inflater.inflate(R.layout.contract_list_fragment, container, false);
+
+		contractListAdapter = new ContractListAdapter(this.getSherlockActivity(),
+				(ListView) layout.findViewById(android.R.id.list));
+		setListAdapter(contractListAdapter);
+		registerForContextMenu(layout.findViewById(android.R.id.list));
+
+		return layout;
+	}
+
+	private void syncWithServer() {
+		if (contractListAdapter != null) {
+			contractListAdapter.syncWithServer(this);
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();		
+
+		syncWithServer();		
+	}
+
+	@Override
+	public void onSyncComplete() {
+		if (getSherlockActivity() != null) {
+			if (getSherlockActivity() != null) {
+				getSherlockActivity().invalidateOptionsMenu();
+			}			
+		}
+	}	
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		final ListItem item = (ListItem) contractListAdapter.getItem(position);		
+	}
+}
