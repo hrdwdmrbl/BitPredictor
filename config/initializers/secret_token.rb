7beeba171e3ba0c52f5b826600313a4b0737dc6b
Marc Beaupre <jack.quack@gmail.com>
2013-03-23 10:04:07 -0400

    FIRST

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitPredictor::Application.config.secret_token = '91ab7146c401bca7d54130a97f7188e290bcb89c10383f2e76d323bc91c28215094afe9273e97e3fb3f77da4be4f15aecdc239ef10d895104a25d23003fd8fe1'
