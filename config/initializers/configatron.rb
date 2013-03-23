276f43686beafec3678b4501e9fa63b855e58467
Marc Beaupre <jack.quack@gmail.com>
2013-03-23 15:42:57 -0400

    oups

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -1,6 +1,6 @@
 require 'configatron'
 
-configatron.development.facebook.app_id = 152613691570503
-configatron.development.facebook.app_secret = '623644277ea911b9dbc695f2c9937932'
-configatron.production.facebook.app_id = 527188943990546
-configatron.production.facebook.app_secret = 'a90cb7138481f1540fad9e3d8ba5b0c1'
+configatron.production.facebook.app_id = 152613691570503
+configatron.production.facebook.app_secret = '623644277ea911b9dbc695f2c9937932'
+configatron.development.facebook.app_id = 527188943990546
+configatron.development.facebook.app_secret = 'a90cb7138481f1540fad9e3d8ba5b0c1'
