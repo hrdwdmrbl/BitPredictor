31af980e9d5dc868de16c1ab278c803d66b8b2e9
Marc Beaupre <jack.quack@gmail.com>
2013-03-23 10:58:56 -0400

    Users models ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BitPredictor::Application.routes.draw do
+  devise_for :users
+
+  root :to => "statics#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
